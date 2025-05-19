a1c75c813faa531e954f3e0e705c568ec120a08e
SAMMILLERR <saatvik.pandey.cd.che22@itbhu.ac.in>
2025-05-20 03:23:41 +0530

    Redirect to authentication page on home component load

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return <div>Hello, world!</div>;
+  redirect("/auth/auth");
+  return null;
 }
